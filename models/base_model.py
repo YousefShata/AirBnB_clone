e796060c5d53562be88506276a754786acfb0e0f
YousefShata <yossef_shata_a@yahoo.com>
2023-10-11 13:41:37 -0700

    Base Model added

---
+++ models/base_model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+This is the Base Model for all other classes
+"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    BaseModel Class which will be inharited
+    """
+
+    def __init__(self):
+        """
+        Initiate instances attributes
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """
+        Representing the object
+        """
+        return "[{}] ({}) {}".format(self.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """
+        save the instance data
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        returns a dictionary containing all keys/values
+        """
+        dic = self.__dict__
+        dic["__class__"] = self.__name__
+        dic["created_at"] = self.created_at.isoformat()
+        dic["updated_at"] = self.updated_at.isoformat()
+
+        return dic
