08e8747c9627cab8fb07b73ffe122d136ef4f1a5
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-10-14 19:53:54 +0300

    test base model

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""unittest to test  base model"""
+from models.base_model import BaseModel
+import unittest
+
+
+class TestBaseModel(unittest.TestCase):
+    """tests for base model"""
+    def setUp(self) -> None:
+        """setup method"""
+        pass
+
+    def test_base(self):
+        """test inputs for base model"""
+        new_model = BaseModel()
+        new_model.name = "bnb"
+        new_model.num = 00
+        self.assertEqual([new_model.name, new_model.num], ["bnb, 00"])
+
+    def test_datetime(self):
+        """
+        Tests for correct datetime format
+        """
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
