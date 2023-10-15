008ba516f5c1ae4d3fb053f06dd7ac81dca0234b
YousefShata <yossef_shata_a@yahoo.com>
2023-10-14 20:06:52 -0700

    some enhancments

--- console.py
+++ console.py
@@ -124,10 +124,10 @@ class HBNBCommand(cmd.Cmd):
                     if search_string in all_values:
                         if len(arg) > 2:
                             if len(arg) > 3:
-                                if (args_array[3] not in
+                                if (args[3] not in
                                         ["created_at", "updated_at", "id"]):
                                     setattr(all_values[search_string], str(
-                                        args[2]), str(arggs[3]))
+                                        args[2]), str(args[3]))
                             else:
                                 print("** value missing **")
                         else:
