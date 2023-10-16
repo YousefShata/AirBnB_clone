73893c137db2a015f5aadf49afd1c7abda694b60
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-10-16 03:38:30 +0300

    test amenity class

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""test amenity class"""
+
+import unittest
+from models.base_model import BaseModel
+from models.amenity import Amenity
+import models.base_model
+import models.amenity
+import datetime
+from time import sleep
+
+
+class TestAmenity(unittest.TestCase):
+    """ class to test city class """
+    def setUp(self):
+        """This method is called before each test method in the test class.
+        """
+        self.new_obj = Amenity()
+
+
+    def test_init_amenity(self):
+        """ test instantiation of class """
+        self.assertEqual(type(self.new_obj.id), str)
+        self.assertEqual(type(self.new_obj.updated_at), datetime.datetime)
+        self.assertEqual(type(self.new_obj.created_at), datetime.datetime)
+
+    def test_save_amenity(self):
+        """ test Amenity.save() """
+        current_updatedAt = self.c.updated_at
+        self.new_obj.save()
+        self.assertNotEqual(current_updatedAt, self.new_obj.updated_at)
+
+        """ test with args """
+        with self.assertRaises(TypeError):
+            self.new_obj.save(13)
+
+    def test_to_dict_amenity(self):
+        """ test Amenity.to_dict() """
+        self.new_obj.name = "NYC"
+        dict_ = self.new_obj.to_dict()
+
+        """ confirming the type of each attr in dict """
+        self.assertEqual(type(dict_['name']), str)
+        self.assertEqual(type(dict_['__class__']), str)
+        self.assertEqual(dict_['__class__'], "Amenity")
+        self.assertEqual(type(dict_['updated_at']), str)
+        self.assertEqual(type(dict_['id']), str)
+        self.assertEqual(type(dict_['created_at']), str)
+
+        """ test with args """
+        with self.assertRaises(TypeError):
+            self.new_obj.to_dict('str')
+
+
+if __name__ == '__main__':
+    unittest.main()
