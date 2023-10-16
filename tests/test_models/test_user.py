4ff7e0b95f2013ef5194746d658b67a99475f2c1
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-10-16 04:17:00 +0300

    fix bugs

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -18,21 +18,21 @@ class TestUser(unittest.TestCase):
     def check_attributes(self):
         """ check for attributes """
         user = User()
-        user.f_name = "bnb"
-        user.l_name = "bnb"
+        user.first_name = "bnb"
+        user.last_name = "bnb"
         user.email = "bnb@email.com"
         user.password = "root"
-        self.assertEqual(user.f_name, "bnb")
-        self.assertEqual(user.l_name, "bnb")
+        self.assertEqual(user.first_name, "bnb")
+        self.assertEqual(user.last_name, "bnb")
         self.assertEqual(user.email, "bnb@email.com")
         self.assertEqual(user.password, "root")
-        self.assertTrue(hasattr(User, "f_name"))
-        self.assertTrue(hasattr(User, "l_name"))
+        self.assertTrue(hasattr(User, "first_name"))
+        self.assertTrue(hasattr(User, "last_name"))
         self.assertTrue(hasattr(User, "password"))
         self.assertTrue(hasattr(User, "email"))
         self.assertEqual(type(User.email), str)
-        self.assertEqual(type(User.f_name), str)
-        self.assertEqual(type(User.l_name), str)
+        self.assertEqual(type(User.first_name), str)
+        self.assertEqual(type(User.last_name), str)
         self.assertEqual(type(User.password), str)
 
     def test_init_user(self):
@@ -52,14 +52,14 @@ class TestUser(unittest.TestCase):
 
     def test_to_dict_city(self):
         """ test User.to_dict() """
-        self.new_obj.f_name = "bnb"
-        self.new_obj.l_name = "bnb"
+        self.new_obj.first_name = "bnb"
+        self.new_obj.last_name = "bnb"
         self.new_obj.email = "jonas@example.com"
         self.new_obj.password = "root"
         dict_ = self.new_obj.to_dict()
-        self.assertEqual(type(dict_['f_name']), str)
-        self.assertEqual(dict_['f_name'], "bnb")
-        self.assertEqual(dict_['l_name'], "bnb")
+        self.assertEqual(type(dict_['first_name']), str)
+        self.assertEqual(dict_['first_name'], "bnb")
+        self.assertEqual(dict_['last_name'], "bnb")
         self.assertEqual(dict_['email'], "jonas@example.com")
         self.assertEqual(dict_['password'], "root")
         self.assertEqual(type(dict_['__class__']), str)
