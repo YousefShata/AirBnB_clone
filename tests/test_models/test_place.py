d85f4098073113199b547f2d62da255e541d8410
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-10-16 03:51:44 +0300

    test place class

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,51 +1,52 @@
 #!/usr/bin/python3
-"""test cases for city class"""
+"""test place class"""
+
 import unittest
 from models.base_model import BaseModel
-from models.city import City
+from models.place import Place
 import models.base_model
-import models.city
+import models.place
 import datetime
 from time import sleep
 
 
-class TestCity(unittest.TestCase):
+class TestPlace(unittest.TestCase):
     """ class to test city class """
     def setUp(self):
         """set up method"""
-        self.new_obj = City()
+        self.new_obj = Place()
 
 
-    def test_init_city(self):
+    def test_init_place(self):
         """ test instantiation of class """
         self.assertEqual(type(self.new_obj.id), str)
         self.assertEqual(type(self.new_obj.updated_at), datetime.datetime)
         self.assertEqual(type(self.new_obj.created_at), datetime.datetime)
 
-    def test_save_city(self):
+    def test_save_place(self):
         """ test State.save() """
         current_updatedAt = self.new_obj.updated_at
         self.new_obj.save()
         self.assertNotEqual(current_updatedAt, self.new_obj.updated_at)
 
-        """ test with args """
+        """ test positional args """
         with self.assertRaises(TypeError):
-            self.new_obj.save(10)
+            self.new_obj.save(13)
 
-    def test_to_dict_city(self):
-        """ test City.to_dict() """
+    def test_to_dict_place(self):
+        """ test BaseModel.to_dict() """
         self.new_obj.name = "bnb"
         dict_ = self.new_obj.to_dict()
 
         """ confirming the type of each attr in dict """
         self.assertEqual(type(dict_['name']), str)
         self.assertEqual(type(dict_['__class__']), str)
-        self.assertEqual(dict_['__class__'], "City")
+        self.assertEqual(dict_['__class__'], "Place")
         self.assertEqual(type(dict_['updated_at']), str)
         self.assertEqual(type(dict_['id']), str)
         self.assertEqual(type(dict_['created_at']), str)
 
-        """ test with args """
+        """ test positional args """
         with self.assertRaises(TypeError):
             self.new_obj.to_dict('str')
 
